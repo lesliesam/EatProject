a32208c49581a96c0031ce6f53e1fc13ccc872e8
Yu Zheng(Sam) <sam@tradehero.mobi>
2016-01-18 15:50:51 +0800

    Wrap the app route into one file.

--- index.android.js
+++ index.android.js
@@ -13,76 +13,12 @@ var {
   BackAndroid,
 } = React;
 
-var NavBar = require('./js/view/NavBar')
-var LandingPage = require('./js/view/LandingPage')
-var LoginPage = require('./js/view/LoginPage')
-var MyHomePage = require('./js/view/MyHomePage')
-var MySettings = require('./js/view/MySettings')
-
-var _navigator;
-BackAndroid.addEventListener('hardwareBackPress', () => {
-  if (_navigator && _navigator.getCurrentRoutes().length > 1) {
-    _navigator.pop();
-    return true;
-  }
-  return false;
-});
-
-var RouteMapper = function(route, navigationOperations, onComponentRef) {
-  _navigator = navigationOperations;
-  var showBackButton = true;
-  if (route.hideBackButton) {
-    showBackButton = false;
-  }
-
-  if (route.name === 'landing') {
-    return (
-      <LandingPage navigator={navigationOperations} />
-    );
-  } else if (route.name === 'login') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="邮件登陆"/>
-        <LoginPage navigator={navigationOperations} />
-      </View>
-    );
-  } else if (route.name === 'myhome') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="我的" />
-        <MyHomePage navigator={navigationOperations} />
-      </View>
-    );
-  } else if (route.name === 'settings') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="设置" showBackButton='true' navigator={navigationOperations}/>
-        <MySettings navigator={navigationOperations} />
-      </View>
-    );
-  }
-};
+var AppNavigator = require('./AppNavigator')
 
 var EatProject = React.createClass({
   render: function() {
     return (
-      // <NavigatorIOS 
-      //     style={styles.container} 
-      //     barTintColor='#1789d5'
-      //     tintColor='#FFFFFF'
-      //     titleTextColor='#FFFFFF'
-      //   initialRoute={{
-      //     title: 'Landing',
-      //     component: LandingPage,
-      //     backButtonTitle: ' ',
-      //     navigationBarHidden: true,
-      // }}/>
-
-      <Navigator
-        style={styles.container}
-        initialRoute={{name: 'landing'}}
-        configureScene={() => Navigator.SceneConfigs.HorizontalSwipeJump}
-        renderScene={RouteMapper} />
+      <AppNavigator />
     );
   }
 });
--- index.ios.js
+++ index.ios.js
@@ -12,74 +12,12 @@ var {
   View,
 } = React;
 
-var NavBar = require('./js/view/NavBar')
-var LandingPage = require('./js/view/LandingPage')
-var LoginPage = require('./js/view/LoginPage')
-var MyHomePage = require('./js/view/MyHomePage')
-var MySettings = require('./js/view/MySettings')
-var MyNotifications = require('./js/view/MyNotifications')
-
-var RouteMapper = function(route, navigationOperations, onComponentRef) {
-  var showBackButton = true;
-  if (route.hideBackButton) {
-    showBackButton = false;
-  }
-
-  if (route.name === 'landing') {
-    return (
-      <LandingPage navigator={navigationOperations} />
-    );
-  } else if (route.name === 'login') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="邮件登陆"/>
-        <LoginPage navigator={navigationOperations} />
-      </View>
-    );
-  } else if (route.name === 'myhome') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="我的" />
-        <MyHomePage navigator={navigationOperations} />
-      </View>
-    );
-  } else if (route.name === 'settings') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="设置" showBackButton='true' navigator={navigationOperations}/>
-        <MySettings navigator={navigationOperations} />
-      </View>
-    );
-  } else if (route.name === 'myNotifications') {
-    return (
-      <View style={{flex: 1}}>
-        <NavBar title="通知" showBackButton='true' navigator={navigationOperations}/>
-        <MyNotifications navigator={navigationOperations} />
-      </View>
-    );
-  }
-};
+var AppNavigator = require('./AppNavigator')
 
 var EatProject = React.createClass({
   render: function() {
     return (
-      // <NavigatorIOS 
-      //     style={styles.container} 
-      //     barTintColor='#1789d5'
-      //     tintColor='#FFFFFF'
-      //     titleTextColor='#FFFFFF'
-      //   initialRoute={{
-      //     title: 'Landing',
-      //     component: LandingPage,
-      //     backButtonTitle: ' ',
-      //     navigationBarHidden: true,
-      // }}/>
-
-      <Navigator
-        style={styles.container}
-        initialRoute={{name: 'landing'}}
-        configureScene={() => Navigator.SceneConfigs.PushFromRight}
-        renderScene={RouteMapper} />
+      <AppNavigator />
     );
   }
 });
